1d75dc48dca275ab9e977f89cf992029b1e22c3c
Glenn Hinks <ghinks@yahoo.com>
2023-01-29 08:33:12 -0500

    feat: initial
    initial commit for implementing plugins with namespace packages based on this [plural sight course](https://app.pluralsight.com/library/courses/core-python-organizing-larger-programs/table-of-contents)

---
+++ core/demo_reader/multireader.py
@@ -0,0 +1,35 @@
+import importlib
+import os
+import pkgutil
+import demo_reader.compressed
+
+def iter_namespace(ns_pkg):
+    return pkgutil.iter_modules(
+        ns_pkg.__path__,
+        ns_pkg.__name__ + ".")
+
+compression_plugins = {
+    importlib.import_module(module_name)
+    for _, module_name, _
+    in iter_namespace(
+        demo_reader.compressed)
+}
+
+extension_map = {
+    module.extension: module.opener
+    for module in compression_plugins
+}
+
+
+class MultiReader:
+    def __init__(self, filename):
+        print(extension_map)
+        extension = os.path.splitext(filename)[1]
+        opener = extension_map.get(extension, open)
+        self.f = opener(filename, 'rt')
+
+    def close(self):
+        self.f.close()
+
+    def read(self):
+        return self.f.read()
